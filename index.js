70c724249454f6f14d0a71db5530b0d9df8500ba
huzefa <huzefaghurna@gmail.com>
2026-06-05 12:20:07 +0530

    test route push

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ app.use("/", (req, res) => {
   return res.json({ mess: "Hello from server V1" });
 });
 
+
+app.use("/test", (req, res) => {
+  return res.json({ mess: "Hello from test route@V1" });
+});
+
+
 app.listen(PORT, () => {
   console.log(`Server is ruiing on ${PORT}`);
 });
