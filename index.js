53f49a214fdcbb8ee8e85dd17d51af0a0179b0bd
huzefa <huzefaghurna@gmail.com>
2026-06-05 14:22:36 +0530

    feat: v3 push

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 import express from "express";
 
 const PORT = process.env.PORT ?? 8080;
-
 const app = express();
 
+/* home route */
 app.use("/", (req, res) => {
-  return res.json({ mess: "Hello from server V2@latest" });
+  return res.json({ mess: "Hello from server Version@3" });
 });
 
-
+/* test route */
 app.use("/test", (req, res) => {
-  return res.json({ mess: "Hello from test route@V1" });
+  return res.json({ mess: "Hello from test route@V3" });
 });
-
 
 app.listen(PORT, () => {
-  console.log(`Server is ruiing on ${PORT}`);
+  console.log(`Server is running on ${PORT}`);
 });
